2bc6fd77dbc8f4b9794bd80fc9ed41b09e002df2
Serge <serge.koval@outlook.com>
2018-03-14 00:27:00 +0100

    added 16 bit timer pwm project

---
+++ Atmega8Programming/16BitTimer_PWM/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * 16BitTimer_PWM.cpp
+ *
+ * Created: 3/14/2018 12:09:05 AM
+ * Author : Serge
+ */ 
+
+ #define  F_CPU 1000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+	DDRB |= (1<<PINB1);
+
+	TCCR1A |= (1<<WGM11);
+	TCCR1B |= (1<<WGM12) | (1<<WGM13);
+
+	TCCR1A |= (1<<COM1A1) | (1<<COM1A0);
+
+	TCCR1B |= (1<< CS10);
+
+	ICR1 = 19999;
+	OCR1A = 17999;
+
+    while (1) 
+    {
+		OCR1A = ICR1 - 800;
+		_delay_ms(500);
+		OCR1A = ICR1 - 2200;
+		_delay_ms(50);
+    }
+}
+
---
+++ Atmega8Programming/ServoControl/main.cpp
@@ -0,0 +1,65 @@
+/*
+ * PWM.cpp
+ *
+ * Created: 3/13/2018 12:10:19 PM
+ * Author : E0475141
+ */ 
+
+ #define  F_CPU 1000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+int fadingDirection = 1;
+
+int main(void)
+{
+	DDRB |= (1<<PINB3);
+
+	// set to fast pwm
+	TCCR2 |= (1<<WGM21) | (1<< WGM20);  
+	/*
+	 Clear OC2 on Compare Match, set OC2 at BOTTOM,
+	 (non-inverting mode)
+	*/
+	TCCR2 |= (1<<COM21);	
+	
+	TCCR2|= (1<<CS22) | (1<<CS20) ;//128 prescale
+	
+	//duty cycle
+	OCR2 = 15; // 156 is 100%
+
+	// second timer setup
+	sei();
+	//setup another timer for interrupts on every 10ms
+	TCCR1B |= (1 << CS10) | (1 << CS11) | (1 << WGM12);
+
+	// Timer Interrupt Mask Register
+	//  OCIE1A: Timer/Counter1, Output Compare A Match Interrupt Enable
+	TIMSK = (1 << OCIE1A );
+	
+	//timer Output Compare Registers
+	OCR1A = 2000;// 15624; // 1 MHz / 64(scale)
+
+	while(1)
+	{
+		
+	}
+
+}
+
+ISR(TIMER1_COMPA_vect)
+{	
+	if(OCR2 == 20)
+	{
+		fadingDirection = -1;
+	}
+	else if (OCR2 == 1)
+	{
+		fadingDirection = 1;
+	}
+
+	OCR2 += (fadingDirection);
+}
+
