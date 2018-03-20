3093902cea3fc05db7daca20b74284d26fa454af
sergiokoval <sergey4j@gmail.com>
2018-03-20 19:58:06 +0100

    added spi

---
+++ Atmega8Programming/SPI/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SPI.cpp
+ *
+ * Created: 3/20/2018 7:57:41 PM
+ * Author : Serge
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
